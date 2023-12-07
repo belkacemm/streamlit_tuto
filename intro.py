6fd9a7dbc44e519bf280d2cd6a36dcc5f2ed4a5c
belkacemm <belkacemmezhoud@comcast.net>
2023-12-06 18:06:46 -0600

    first commit

---
+++ intro.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Streamlit")
+st.header("Introduction")
+st.subheader("set up")
+st.text("this introduction will show you how you get ready for using streamlit framework")
+st.checkbox("show/hide")
